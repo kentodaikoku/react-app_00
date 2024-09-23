f457e099d7bc94d092598990aa1e6d2471da67ef
kentodaikoku <kedks090597@gmail.com>
2024-09-23 23:09:18 +0800

    ✨ Login UI

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,18 @@
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 import { FC, memo } from "react";
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: FC = memo(() => {
-  return <p>this is login page</p>
+  return (
+    <Flex align='center' justify='center' height='100vh'>
+      <Box bg='white' p={4} w='sm' borderRadius='md' shadow='md'>
+        <Heading as='h1' size='lg' textAlign='center'>User Management APP</Heading>
+        <Divider my={4} />
+        <Stack spacing={6} px={10} py={4}>
+          <Input placeholder="User ID" />
+          <PrimaryButton>Log in</PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 })
