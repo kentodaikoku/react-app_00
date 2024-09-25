5c765279fa3cd06bb041a3bd546e0e4b81b30452
kentodaikoku <kedks090597@gmail.com>
2024-09-25 22:33:31 +0800

    ✨ Login Message feature

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,10 +2,12 @@ import axios from "axios"
 import { useCallback, useState } from "react"
 import { User } from "../types/api/user"
 import { useNavigate } from "react-router-dom"
+import { useMessage } from "./useMessage"
 
 export const useAuth = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false); // Loading Setting
+  const showMessage = useMessage();
 
   const login = useCallback((id: string) => {
     setLoading(true);
@@ -13,15 +15,17 @@ export const useAuth = () => {
     // JSON Placeholder　 // REF; async/awaitの記述
     axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => {
       if (res.data) {
+        showMessage({title: "Login Success", status: 'success'});
         navigate("/home");
       } else {
-        alert("No User found");
+        showMessage({title: "Login Unsuccess: No user found", status: 'error'});
       }
     }).catch((error) => {
       console.error(`Login Error: ${error}`);
-      alert("There is an error");
+      showMessage({title: "Login Error", status: 'error'});
     }).finally(() => setLoading(false));
-  }, [navigate])
+
+  }, [navigate, showMessage]);
 
   return {login, loading};
 }
